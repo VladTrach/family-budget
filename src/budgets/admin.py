a93632e65c27be05e0c6e3c0ef32c08302c7115a
VladTrach <trach.vladyslav@gmail.com>
2022-10-03 21:51:56 +0200

    Add the budget and transaction models to the django admin.

--- src/budgets/admin.py
+++ src/budgets/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from budgets.models import Budget, Transaction
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
